2cfba4b4fad4209a4192b2a63ca3b9f04e47e72c
Syed Yakub <syedyakub1221@gmail.com>
2026-04-29 18:57:21 +0530

    ci cd setup

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('CI/CD Pipeline Working 🚀');
+  res.send('CI/CD pipeline working 🚀');
 });
 
-app.listen(3000, () => {
+app.listen(3000, '0.0.0.0', () => {
   console.log('Server running on port 3000');
 });
